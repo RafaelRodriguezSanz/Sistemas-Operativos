1cd61d57249dba19bf7496fa670c71feba25448d
RafaelRodriguezSanz <rafaelrodriguezsanz@gmail.com>
2022-04-28 23:10:23 -0300

    minor fixes

--- sistemasoperativos/src/main/java/com/ucudal/tarea1/passwordPrompt.java
+++ sistemasoperativos/src/main/java/com/ucudal/tarea1/passwordPrompt.java
@@ -7,18 +7,29 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 
 public class passwordPrompt {
-
     @FXML
     private Button cancelBtn;
 
     @FXML
+    private Label label;
+
+    @FXML
+    private ImageView lockImage;
+
+    @FXML
     private Button loginBtn;
+
+    @FXML
+    private Text message;
 
     @FXML
     private PasswordField password;
@@ -28,9 +39,11 @@ public class passwordPrompt {
 
     @FXML
     void Login(ActionEvent event) {
+        Scene prompt = App.getScene();
+        TextArea message = (TextArea) prompt.lookup("#message");
+        message.setVisible(false);
         if (OS.checkSudo(this.userNameID.getText(), this.password.getText())) {
-            Node prompt = (Node) event.getSource();
-            prompt.getScene().getWindow().hide();
+            prompt.getWindow().hide();
             Scene main = App.getScene();
 
             TextArea console1 = (TextArea) main.lookup("#Console1");
@@ -38,10 +51,10 @@ public class passwordPrompt {
             console1.appendText("User Loging successfully!" + '\n');
             console2.appendText("User Loging successfully!" + '\n');
             TabPane all = (TabPane) main.lookup("#all");
-            all.setDisable(true);
-
+            all.setDisable(false);
         } else {
 
+            message.setVisible(true);
         }
     }
 
