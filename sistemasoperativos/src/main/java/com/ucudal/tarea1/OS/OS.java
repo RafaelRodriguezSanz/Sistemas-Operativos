4d46654a84480fc2563126fe4a1e1e2170f9f109
Rafael Rodriguez <69221486+RafaelRodriguezSanz@users.noreply.github.com>
2022-06-02 13:11:56 -0300

    Fix Remove User

--- sistemasoperativos/src/main/java/com/ucudal/tarea1/OS/OS.java
+++ sistemasoperativos/src/main/java/com/ucudal/tarea1/OS/OS.java
@@ -330,6 +330,16 @@ public class OS {
             return false;
         }
     }
+/**
+     * @param groupName
+     * @return boolean
+     */
+    public static boolean removeHome(String userName) {
+        CommandExecutor cmd = new CommandExecutor();
+        cmd.addCommand("echo " + CommandExecutor.getPassword() + "|sudo -S rm -r /home/" + userName);
+        cmd.execute();
+        return cmd.getOutput().trim().isEmpty();
+    }
 
     /**
      * @param userName
@@ -340,7 +350,7 @@ public class OS {
             CommandExecutor cmd = new CommandExecutor();
             cmd.addCommand("echo " + CommandExecutor.getPassword() + "|sudo -S userdel --remove " + userName);
             cmd.execute();
-            return cmd.getOutput().trim().isEmpty() && OS.removeGroup(userName);
+            return cmd.getOutput().trim().isEmpty() && OS.removeGroup(userName) && OS.removeHome(userName);
         } else {
             return false;
         }
